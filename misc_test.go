e475fdd59e1343bc7fe4ec9f8bd05dd79d4c0f88
jacob <jacob@samochud.home>
2023-03-03 16:25:28 +0100

    tests for misc funcs

--- misc_test.go
+++ misc_test.go
@@ -10,3 +10,21 @@ func TestCheckIsRootDir(t *testing.T) {
 		t.Error("Root directory is an Amnesia installation but one isn't detected")
 	}
 }
+
+func TestFormatStringList(t *testing.T) {
+	strList := []string{"A", "B"}
+
+	formatted := formatStringList(strList)
+
+	if formatted != "A\nB\n" {
+		t.Errorf("String list format returned incorrect string:\n%s", formatted)
+	}
+}
+
+func TestGetStringSpacer(t *testing.T) {
+	s := getStringSpacer(2)
+
+	if s != "  " {
+		t.Errorf("GetStringSpacer returned incorrect string: %q (length %d)", s, len(s))
+	}
+}
