fbe4ce97b430728c0f2da69d979059f3ed6db947
jacob <jacob@samochud.home>
2023-03-10 15:10:09 +0100

    build windows app without launching terminal

--- winbuild.sh
+++ winbuild.sh
@@ -1,1 +1,1 @@
-CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows go build -o bin/modmanager.txt -ldflags "-s -w" . &&  zip -e ./bin/modmanager.zip ./bin/modmanager.txt
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows go build -o bin/modmanager.exe -ldflags "-s -w -H=windowsgui" . &&  zip -e ./bin/modmanager.zip ./bin/modmanager.txt
